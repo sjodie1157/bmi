afba934a57570bddb2ecd8321e562752a2f5b4bd
sjodie1157 <sjodie1157@gmail.com>
2023-11-13 23:22:44 +0200

    added if statments in javascript

--- script/code.js
+++ script/code.js
@@ -1,15 +1,24 @@
 let weight = document.querySelector('#weighthInput')
 let height = document.querySelector('#HeightInput')
-let age = document.querySelector('#ageInput')
 
+let state = document.querySelector('#status')
 let bmilable = document.querySelector('#bmie')
 
-
 function calculation(){
-    bmilable.textContent = eval( `${weight.value} / (${height.value/100}**2)` ).toFixed(2) + " BMI" 
+    bmilable.textContent = eval( `${weight.value} / (${height.value/100}**2)` ).toFixed(2) + " BMI";
+    state.textContent = statusBmi(state)
+    function statusBmi (state) {
+        if (state < 18.5){
+            return 'underweight';
+        } else if (state < 25){
+            return 'normal';
+        } else if (state < 30){
+            return 'overweight';
+        } else {
+            return 'obese';
+        }
+    }
 }
 
 let calcbtn = document.querySelector('#answer')
 calcbtn.addEventListener('click',calculation)
-
-
